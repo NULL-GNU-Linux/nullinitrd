5844512c69791bf203f86036754aa63973a867f1
mostypc123 <mostypc123@redroselinux.org>
2026-06-21 16:33:13 +0200

    hooks templates

--- src/hooks.hpp
+++ src/hooks.hpp
@@ -7,17 +7,19 @@ namespace fs = std::filesystem;
 
 class HookManager {
 public:
-    HookManager(const Config& cfg, const fs::path& work, 
+    HookManager(const Config& cfg, const fs::path& work,
                 const std::string& kver, bool verbose);
-    
+
     void run_hook(const std::string& hook_name);
-    
+
 private:
     const Config& config;
     fs::path work_dir;
     std::string kernel_version;
     bool verbose;
-    
+
     void run_script(const fs::path& script);
     bool find_and_run(const std::string& hook_name);
 };
+
+void create_hook_template(const std::string& hook_name);
